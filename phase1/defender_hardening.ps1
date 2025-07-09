37dd5dc834494d4b58f7d5490045ed691e88836b
Collin George <grecoast@proton.me>
2025-07-08 20:29:18 -0700

    Create defender_hardening.ps1

---
+++ phase1/defender_hardening.ps1
@@ -0,0 +1,7 @@
+# Cyber Immune System: Windows Defender Hardening Script
+Set-MpPreference -PUAProtection Enabled
+Set-MpPreference -DisableRealtimeMonitoring $false
+Set-MpPreference -MAPSReporting Advanced
+Set-MpPreference -SubmitSamplesConsent 1
+Set-MpPreference -CloudProtectionLevel High
+Set-MpPreference -EnableControlledFolderAccess Enabled
