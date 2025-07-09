6148af82e19f7770d87030c336729cab6a9faca8
Collin George <grecoast@proton.me>
2025-07-08 20:37:16 -0700

    Delete defender_hardening.ps1

--- defender_hardening.ps1
+++
@@ -1,8 +0,0 @@
-
-# Cyber Immune System: Windows Defender Hardening Script
-Set-MpPreference -PUAProtection Enabled
-Set-MpPreference -DisableRealtimeMonitoring $false
-Set-MpPreference -MAPSReporting Advanced
-Set-MpPreference -SubmitSamplesConsent 1
-Set-MpPreference -CloudProtectionLevel High
-Set-MpPreference -EnableControlledFolderAccess Enabled
